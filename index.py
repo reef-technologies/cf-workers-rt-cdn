f1c00ac034a26a9bb55b6d85c3de8b3908c15d2e
Maciej Lech <maciej.lech@reef.pl>
2020-09-22 12:46:07 +0200

    Fix for white list

--- index.py
+++ index.py
@@ -9,53 +9,67 @@
 ######################################################################
 
 
-from .settings import config
+from dataclasses import dataclass
+
+
+@dataclass(frozen=True)
+class Config:
+    CDN_ALLOWED_HOSTS = JSON.parse(CDN_ALLOWED_HOSTS)
+    CDN_PREFIX = CDN_PREFIX
+
+
+config = Config()
 
 
 def log(*args):
     console.log(*args)
 
 
-
-class SimpleRequest:
+class CacheEndpoint:
     def __init__(self, request, url):
         self.request = request
         self.url = url
+        log('CacheEndpoint url:', self.url)
 
     async def fetch(self, response):
         return await fetch(__new__(Request(self.url, self.request)))
 
 
+class CdnError(Exception):
+    def __init__(self, message, status_code=500):
+        self.message = message
+        self.status_code = status_code
+
+
 class Cdn:
     def __init__(self, request):
         self.request = request
+        log('Cdn request:', self.request)
+        self.endpoint = self._parse_endpoint(self.request)
+        log('Cdn endpoint:', self.endpoint)
 
-        self.url = self._parse_url(self.request)
-        log(self.request.url, "url:", self.url)
-
-    def requests(self):
-        yield SimpleRequest(self.request, self.url)
+    def fetch(self):
+        return self.endpoint.fetch()
 
     @classmethod
-    def _parse_url(cls, request):
+    def _parse_endpoint(cls, request):
         url = __new__(URL(request.url))
-
-        if url.host not in config.CDN_ALLOWED_HOSTS:
-            raise Exception('host is not allowed')
 
         new_url = request.url[len(url.origin + config.CDN_PREFIX):]
         if not new_url.startswith('https:') and not new_url.startswith('http:'):
             new_url = url.origin + '/' + new_url
 
-        return __new__(URL(new_url))
+        new_url = __new__(URL(new_url))
+
+        if new_url.host not in config.CDN_ALLOWED_HOSTS:
+            raise CdnError('host is not allowed: ' + new_url.host, 403)
+
+        return CacheEndpoint(request, new_url)
 
 
 async def handleRequest(request):
     cdn = Cdn(request)
-    response = None
-    for request in cdn.requests():
-        response = await request.fetch(response)
-    return response
+    return cdn.fetch()
 
 
 async def handleEvent(event):
@@ -69,7 +83,7 @@ async def handleEvent(event):
         else:
             error = str(exc)
         return __new__(Response(JSON.stringify({'error': error}), {
-            'status': 500,
+            'status': exc.status_code or 500,
             'headers': {'content-type' : 'text/json'},
         }))
 
